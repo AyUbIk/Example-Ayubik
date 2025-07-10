3b8734d1b1af2fbdeadb490293cc125877449d70
AyUbIk <ayubik39@gmail.com>
2025-07-10 12:57:17 +0500

    Update server.js

--- minecraft-server-backend/server.js
+++ minecraft-server-backend/server.js
@@ -1,1 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ Подключено к MongoDB'))
+    .catch(err => console.error('❌ MongoDB error:', err));
+
+app.use('/api/auth', authRoutes);
+
+app.listen(3000, () => {
+    console.log('🚀 Сервер запущен на http://localhost:3000');
+});
