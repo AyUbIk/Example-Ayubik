5adec81f1198ec6a1d152dc47016b7932dfa801f
AyUbIk <ayubik39@gmail.com>
2025-07-10 12:45:17 +0500

    Update User.js

--- minecraft-server-backend/models/User.js
+++ minecraft-server-backend/models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    balance: { type: Number, default: 0 },
+    rank: { type: String, default: 'player' }
+});
+
+module.exports = mongoose.model('User', userSchema);
